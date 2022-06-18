62fbd4e4c41a2cb5714a4d1e33a21fa6450973a4
alam_ridoy <alamridoy7@gmail.com>
2022-06-18 11:29:21 +0600

    list

---
+++ list.py
@@ -0,0 +1,49 @@
+# # mylist = ["apple","ball","cat","dog","elepent"]  
+
+
+# # # mylist2 = [10,True,"lily","lily"]
+# # # print(mylist2)
+
+# # # a = mylist[-3:-1]
+# # # print(a) 
+
+# # # for i in mylist:
+# # #     print(i) 
+
+# # # if "lemon" in mylist:
+# # #     print("yes")
+# # # else: 
+# # #     print("no")
+
+# # mylist.append("strawberry")
+
+# # mylist.insert(2, "cow")
+
+# # mylist.remove("cat")
+# # mylist.pop()
+# # mylist.reverse()
+# # # print(mylist)
+
+# # mynum = [22,9,11,-4,29,33,17]
+# # mynum.sort()
+# # mynum.reverse() 
+# # print(mynum)
+
+# # newlist = [1]*5
+# # print(newlist)
+
+# # list = newlist + mynum
+# # print(list)
+
+# # mynum[::-1]
+# # print(mynum)
+
+# # cy_list = mylist
+
+# # cy_list[1] = "Dhaka"
+# # print(cy_list)
+
+# list = [2,4,8,5,3,5,9]
+
+# newList = [i*i for i in list]
+# print(newList)
---
+++ tuples.py
@@ -0,0 +1,31 @@
+# mytuple = ('r','k','s','m','q')
+# print(mytuple)
+
+# print(mytuple)
+# for i in mytuple:
+#     print(i)
+
+# if  "hasan" in mytuple:
+#     print("yes ")
+# else:
+#     print("no")
+
+# print(mytuple.count('m'))
+# print(mytuple.index('q'))
+# mylist = list(mytuple)
+# print(mylist)
+# mytuple2 = tuple(mylist)
+# print(mytuple2)
+
+# slice = mytuple[1:3-]
+# print(slice)
+
+# newtuple =("ridoy",22,"Dhaka")
+# name,age,city = newtuple
+# print(name)
+
+my_tuple = (0,1,2,3,4,5)
+i1, i2,*i3 = my_tuple
+print(i1)
+print(i2)
+print(i3)
