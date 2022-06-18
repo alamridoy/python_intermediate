bfca771e783a5c748717077b0c372054b6f7293e
alam_ridoy <alamridoy7@gmail.com>
2022-06-18 22:19:05 +0600

    dict

---
+++ dictionaries.py
@@ -0,0 +1,47 @@
+
+
+
+# mydict = {
+#         "name": "Ridoy",
+#          "age": 22,
+#          "city": "Dhaka"
+#          }
+
+
+# mydict2 = dict(name = "ridoy", age = 22, city = "Dhaka" ,email = "ridoy@gmail.com")
+# mydict.update(mydict2)
+# print(mydict)
+# print(mydict2)
+
+# mydict ["Email"] = "alamridoy@gmail.com"
+
+
+# mydict ["Email"] = "alam@gmail.com"
+
+# del mydict["Email"]
+
+
+# mydict.popitem()
+# print(mydict)
+
+# try:
+       
+#        print(mydict["abc"]) 
+       
+# except :
+#      print("This is error , this value is not assignable")
+
+
+# for key , value in mydict.items():  # dict looping
+#     print(key, value)
+
+# mydict_copy = mydict.copy()
+# mydict_copy["email"] = "abc@gmail.com"
+# print(mydict_copy)
+# print(mydict)
+
+mydict ={3:9 , 7:2, 8:1}
+
+mytuple = (6,8)
+mydict3 = {mytuple : 10}
+print(mydict3)
