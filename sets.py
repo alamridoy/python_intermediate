70c9cb1e393afca9ed6c45541cb088fe591bd798
alam_ridoy <alamridoy7@gmail.com>
2022-06-19 10:22:02 +0600

    sets

---
+++ sets.py
@@ -0,0 +1,49 @@
+# myset = {2,3,5657,345,6,435,'ridoy','ridoy`'}
+# print(myset)
+
+# myset2 = set("hello")
+# print(myset2)
+# myset  = set()
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+# myset.add(4)
+# myset.add(5)
+# myset.pop()
+# myset.pop()
+# myset.remove(3)
+# print(myset) 
+
+# myset = {3,4,674,23,4,'ridoy'}
+
+# # for i in myset:
+# #     print(i)
+
+# # myset.update({88})
+# # myset.discard(3)
+
+# # print(myset)
+# myset2 = {3,4,674,23,4,'ridoy',00}
+# print(myset.difference(myset2))
+
+# odd = {1,3,5,7,9}
+# even = {0,2,4,6,8,9}
+# primes = {2,3,5,7}
+
+# union = odd.union(even)
+# int = odd.intersection(even)
+# print(int)
+# print(union)
+# setB = {2,3,4,56,67}
+# setA = {2,3,4,56,67,99,88}
+# # diff = setB.symmetric_difference(setA)
+# # print(diff)
+# # setA.symmetric_difference_update(setB)
+# # print(setA)
+# # 
+# # 
+# # 
+# setCopy = setA.copy()
+# setA.add(000)
+# print(setCopy)
+# print(setA)
---
+++ strings.py
@@ -0,0 +1,51 @@
+# str= """i am \
+# a \
+# programmer"""
+# print(str)
+
+# str = "Hello Dhaka"
+
+# sli = str[1:7]
+# print(sli)
+
+# str = "Hello Dhaka" 
+# str2 = "dhaka"
+
+# s = str +" "+ str2
+# print(s)
+# for i in str:
+#     print(i)
+
+# if "u" in str:
+#     print("yes")
+# str = str.strip()
+# print(str.upper())
+# print(str.lower())
+# print(str.startswith('He'))
+# print(str.find('l'))
+# print(str.count('l '))
+# print(str.replace("Dhaka", "World"))
+# print(str.split())
+# str2 = "How about you?"
+
+# print("-".join(str2))
+
+# mylist = ['a'] * 5
+
+# # my_str = ""
+# # for i in mylist:
+# #     my_str += i
+
+# my_str = "1".join(mylist)
+# print(my_str)
+
+# var = 3.141577867
+# str = "This value is %.2f" % var
+# print(str)
+
+var1 = 3.14
+var2 = 9.22
+# var3 = 'This value is {} {}'.format(var1*2,var2)
+# print(var3)
+var3 = f'This value is {var1} and {var2}'
+print(var3)
